480b221fa5bd36ee3fe312399a16a06fa030fd5d
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2024-10-17 00:36:36 +0530

    Updating Content

--- SUV Sales.R
+++ SUV Sales.R
@@ -7,26 +7,21 @@ suv_data$rel_Freq <- suv_data$sales/total_sales
 suv_data$rel_Freq_Percent <- 100*(suv_data$sales/total_sales)
 suv_data$cumulative_Freq <- cumsum(suv_data$rel_Freq)
 suv_data$cumulative_Freq_Percent <- 100*suv_data$cumulative_Freq
-
 # Bar Chart
 barplot(suv_data$sales~suv_data$brand, xlab = "brand" , ylab = "units sold",
         main = "Bar Plot - Top 5 SUVs in India")
-
 # Pareto Chart
 suv_2 <- suv_data[order(suv_data$sales, decreasing = TRUE),]
 barplot(suv_2$sales,names.arg = suv_2$brand,main = "Pareto Plot - Top 5 SUV's in India")
-
 # pie chart
 pie(suv_data$rel_Freq, suv_data$brand, main="Pie Chart - Top 5 SUVs in India")
 
 colours <- c("green","yellow","red","orange")
 pie(suv_data$rel_Freq_Percent,brand,main = "Pie Chart" , col=colours )
-
 ## Dot Plot
 Age <- c(52,21,43,52,67,43,63,35,43,52,21,35,21,21,43,35,60,43,35)
 plot(Age)
 dotchart(Age, main = "Dot Plot of Age", xlab = "Age", xlim = c(20,70))
-
 ## stem and leaf plot
 stem(Age)
 
