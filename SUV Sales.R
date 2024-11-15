a97f2c78f2d96e58eeb3e7d0ffd0eab762c689aa
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2024-11-15 21:29:49 +0530

    Updating Content

--- SUV Sales.R
+++ SUV Sales.R
@@ -2,11 +2,6 @@ total_sales <- 485356
 brand <- c("Tata Puch","Hyundai Creta","Maruti Brezza","Mahindra Scorpio",
            "Tata Nexon","Others")
 sales <- c(72466,63752,59147,53068,48593,188330)
-
-
-
-
-
 suv_data <- data.frame(brand,sales)
 suv_data$rel_Freq <- suv_data$sales/total_sales
 suv_data$rel_Freq_Percent <- 100*(suv_data$sales/total_sales)
@@ -19,6 +14,7 @@ barplot(suv_data$sales~suv_data$brand, xlab = "brand" , ylab = "units sold",
         main = "Bar Plot - Top 5 SUVs in India")
 
 # Pareto Chart
+
 suv_2 <- suv_data[order(suv_data$sales, decreasing = TRUE),]
 barplot(suv_2$sales,names.arg = suv_2$brand,main = "Pareto Plot - Top 5 SUV's in India")
 
