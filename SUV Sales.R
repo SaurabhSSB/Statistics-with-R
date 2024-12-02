8163a66175de7927d627b6e92cd8f6ae06c61537
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2024-12-02 22:16:23 +0530

    Updating Content

--- SUV Sales.R
+++ SUV Sales.R
@@ -5,13 +5,9 @@ sales <- c(72466,63752,59147,53068,48593,188330)
 suv_data <- data.frame(brand,sales)
 suv_data$rel_Freq <- suv_data$sales/total_sales
 suv_data$rel_Freq_Percent <- 100*(suv_data$sales/total_sales)
-
-
-
-
-
 suv_data$cumulative_Freq <- cumsum(suv_data$rel_Freq)
 suv_data$cumulative_Freq_Percent <- 100*suv_data$cumulative_Freq
+
 # Bar Chart
 
 barplot(suv_data$sales~suv_data$brand, xlab = "brand" , ylab = "units sold",
