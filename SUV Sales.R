faa96589dabbe42a2075d876e90ef41e75494013
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2024-12-08 03:44:38 +0530

    Updating Content

--- SUV Sales.R
+++ SUV Sales.R
@@ -3,14 +3,11 @@ brand <- c("Tata Puch","Hyundai Creta","Maruti Brezza","Mahindra Scorpio",
            "Tata Nexon","Others")
 sales <- c(72466,63752,59147,53068,48593,188330)
 suv_data <- data.frame(brand,sales)
-
-
-
-
 suv_data$rel_Freq <- suv_data$sales/total_sales
 suv_data$rel_Freq_Percent <- 100*(suv_data$sales/total_sales)
 suv_data$cumulative_Freq <- cumsum(suv_data$rel_Freq)
 suv_data$cumulative_Freq_Percent <- 100*suv_data$cumulative_Freq
+
 # Bar Chart
 
 barplot(suv_data$sales~suv_data$brand, xlab = "brand" , ylab = "units sold",
