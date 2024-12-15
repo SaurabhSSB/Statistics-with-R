306c8bfe33624ce7d170c3018ccd267bfba2150f
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2024-12-15 20:17:37 +0530

    Updating Content

--- Operations on Titanic Sheet.R
+++ Operations on Titanic Sheet.R
@@ -1,18 +1,19 @@
 library(readxl)
-
 df<-read_excel("C:/Users/Hp/Desktop/titanic.xlsx")
-
 head(df,10)
-
 str(df)
-
 summary(df)
 
 df[2,]
+
 df[1:5,]
+
 df[,6]
+
 df[,6:8]
+
 df[3:5,1:5]
+
 df$gender
 
 summary(df$survived)
